9bf20eca5dd827653cb34672f7839311706a1d41
CHAYMAE EL HARKAOUI <chaimaechaimae197@gmail.com>
2024-04-29 05:04:55 +0000

    updates

--- src/Pages/Client_Space_Page.java
+++ src/Pages/Client_Space_Page.java
@@ -40,7 +40,7 @@ public class Client_Space_Page {
 	    int lb1_height = (int) ((40*frame.getHeight())/600);
 	    int lb1_font_size = (int) ((17*frame.getWidth())/900);
         Custom_Label lab1 = new Custom_Label(lb1_x, lb1_y, lb1_width, lb1_height,
-        "Menu","Arial",lb1_font_size,"#2F5597");
+        "Menu","Segoe Print",lb1_font_size,"#2F5597");
         lab1.setHorizontalAlignment(SwingConstants.CENTER);
         lab1.setBackground(Color.decode("#b7c9e2"));
         lab1.setOpaque(true);
@@ -48,6 +48,8 @@ public class Client_Space_Page {
         
         // color
         String color = "#002F5E";
+        // font
+        String font = "calibri";
        
         
         // profile button 
@@ -57,7 +59,7 @@ public class Client_Space_Page {
 	    int bt1_height = (int) ((40*frame.getHeight())/600);
 	    int bt1_font_size = (int) ((12*frame.getWidth())/900);
         Custom_Button profile = new Custom_Button(bt1_x,bt1_y,bt1_width,bt1_height,
-        "Profile","#FFFFFF","Segoe Print",bt1_font_size,false);
+        "Profile","#FFFFFF",font,bt1_font_size,false);
         profile.setBackground(Color.decode("#2F5597"));
         profile.setOpaque(true);
         profile.change_style("#b39700","#FFFFFF");
@@ -73,7 +75,7 @@ public class Client_Space_Page {
 	    int bt2_height = (int) ((40*frame.getHeight())/600);
 	    int bt2_font_size = (int) ((12*frame.getWidth())/900);
         Custom_Button products = new Custom_Button(bt2_x,bt2_y,bt2_width,bt2_height,
-        "Products","#FFFFFF","Segoe Print",bt2_font_size,false);
+        "Products","#FFFFFF",font,bt2_font_size,false);
         products.setBackground(Color.decode("#2F5597"));
         products.setOpaque(true);
         products.change_style("#b39700","#FFFFFF");
@@ -89,7 +91,7 @@ public class Client_Space_Page {
 	    int bt3_height = (int) ((40*frame.getHeight())/600);
 	    int bt3_font_size = (int) ((12*frame.getWidth())/900);
         Custom_Button orders = new Custom_Button(bt3_x,bt3_y,bt3_width,bt3_height,
-        "Orders","#FFFFFF","Segoe Print",bt3_font_size,false);
+        "Orders","#FFFFFF",font,bt3_font_size,false);
         orders.setBackground(Color.decode("#2F5597"));
         orders.setOpaque(true);
         orders.change_style("#b39700","#FFFFFF");
@@ -105,7 +107,7 @@ public class Client_Space_Page {
 	    int bt4_height = (int) ((40*frame.getHeight())/600);
 	    int bt4_font_size = (int) ((12*frame.getWidth())/900);
         Custom_Button cart = new Custom_Button(bt4_x,bt4_y,bt4_width,bt4_height,
-        "Cart","#FFFFFF","Segoe Print",bt4_font_size,false);
+        "Cart","#FFFFFF",font,bt4_font_size,false);
         cart.setBackground(Color.decode("#2F5597"));
         cart.setOpaque(true);
         cart.change_style("#b39700","#FFFFFF");
@@ -121,7 +123,7 @@ public class Client_Space_Page {
 	    int bt5_height = (int) ((40*frame.getHeight())/600);
 	    int bt5_font_size = (int) ((12*frame.getWidth())/900);
         Custom_Button favorites = new Custom_Button(bt5_x,bt5_y,bt5_width,bt5_height,
-        "Favorites","#FFFFFF","Segoe Print",bt5_font_size,false);
+        "Favorites","#FFFFFF",font,bt5_font_size,false);
         favorites.setBackground(Color.decode("#2F5597"));
         favorites.setOpaque(true);
         favorites.change_style("#b39700","#FFFFFF");
@@ -137,7 +139,7 @@ public class Client_Space_Page {
 	    int bt6_height = (int) ((40*frame.getHeight())/600);
 	    int bt6_font_size = (int) ((12*frame.getWidth())/900);
         Custom_Button log_out = new Custom_Button(bt6_x,bt6_y,bt6_width,bt6_height,
-        "Log out","#FFFFFF","Segoe Print",bt6_font_size,false);
+        "Log out","#FFFFFF",font,bt6_font_size,false);
         log_out.setBackground(Color.decode("#2F5597"));
         log_out.setOpaque(true);
         log_out.change_style("#b39700","#FFFFFF");
